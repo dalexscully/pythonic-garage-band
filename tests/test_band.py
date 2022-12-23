7c6807ded7d06904514d4b17cad2c24267b25be0
Domaine Scully <dalexscully@gmail.com>
2022-12-22 19:01:10 -0500

    Lab complete

--- tests/test_band.py
+++ tests/test_band.py
@@ -1,28 +1,15 @@
-import json
 import pytest
-#import yaml
 
-from pythonic_garage_band.band import Band, Musician, Guitarist, Bassist, Drummer
+from pythonic_garage_band.band import (
+    Band,
+    Musician,
+    Guitarist,
+    Bassist,
+    Drummer,
+)
 
 
-def test_band_name():
-    nirvana = Band("Nirvana", [])
-
-    assert nirvana.name == "Nirvana"
-
-def test_band_str():
-    nirvana = Band("Nirvana", [])
-    actual = str(nirvana)
-    expected = "The band Nirvana"
-    assert actual == expected
-
-def test_band_repr():
-    nirvana = Band("Nirvana", [])
-    actual = repr(nirvana)
-    expected = "Band instance. name=Nirvana, members=[]"
-    assert actual == expected
-
-# @pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_guitarist_str():
     joan = Guitarist("Joan Jett")
     actual = str(joan)
@@ -30,7 +17,7 @@ def test_guitarist_str():
     assert actual == expected
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_guitarist_repr():
     joan = Guitarist("Joan Jett")
     actual = repr(joan)
@@ -38,7 +25,7 @@ def test_guitarist_repr():
     assert actual == expected
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_drummer_str():
     sheila = Drummer("Sheila E.")
     actual = str(sheila)
@@ -46,7 +33,7 @@ def test_drummer_str():
     assert actual == expected
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_drummer_repr():
     sheila = Drummer("Sheila E.")
     actual = repr(sheila)
@@ -54,7 +41,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -62,7 +49,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
@@ -70,30 +57,35 @@ def test_bassist_repr():
     assert actual == expected
 
 
+# @pytest.mark.skip("todo")
+def test_band_name():
+    nirvana = Band("Nirvana", [])
+
+    assert nirvana.name == "Nirvana"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_guitarist():
     jimi = Guitarist("Jimi Hendrix")
     assert jimi.name == "Jimi Hendrix"
     assert jimi.get_instrument() == "guitar"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_bassist():
     flea = Bassist("Flea")
     assert flea.name == "Flea"
     assert flea.get_instrument() == "bass"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_drummer():
     ginger = Drummer("Ginger Baker")
     assert ginger.name == "Ginger Baker"
     assert ginger.get_instrument() == "drums"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_instruments(one_band):
     instruments = ["guitar", "bass", "drums"]
     for i, member in enumerate(one_band.members):
@@ -101,7 +93,7 @@ def test_instruments(one_band):
         assert member.get_instrument() == instruments[i]
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_individual_solos(one_band):
     for member in one_band.members:
         if member.get_instrument() == "guitar":
@@ -112,7 +104,7 @@ def test_individual_solos(one_band):
             assert member.play_solo() == "rattle boom crash"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_band_members(one_band):
 
     assert len(one_band.members) == 3
@@ -130,7 +122,7 @@ def test_band_members(one_band):
     assert one_band.members[2].name == "Dave Grohl"
 
 
-#@pytest.mark.skip("pending")
+# @pytest.mark.skip("todo")
 def test_play_solos_for_whole_band(one_band):
     solos = one_band.play_solos()
     assert len(solos) == 3
@@ -139,11 +131,6 @@ def test_play_solos_for_whole_band(one_band):
     assert solos[2] == "rattle boom crash"
 
 
-#@pytest.mark.skip("pending")
-def test_to_list():
-    assert Band.to_list() == []
-    Band("The Nobodies", [])
-    assert len(Band.to_list()) == 1
 
 
 #######################
@@ -165,16 +152,22 @@ def nirvana_data():
 
 @pytest.fixture
 def one_band():
-    some_band = Band(
-        "Nirvana",
-        [Guitarist("Kurt Cobain"), Bassist("Krist Novoselic"), Drummer("Dave Grohl"),],
-    )
+    members = [
+        Guitarist("Kurt Cobain"),
+        Bassist("Krist Novoselic"),
+        Drummer("Dave Grohl"),
+    ]
+
+    some_band = Band("Nirvana", members)
+
     return some_band
 
 
 @pytest.fixture(autouse=True)
 def clean():
-    """runs before each test automatically
+    """runs before each test automatically.
+    This is necessary because otherwise band instances added in one test
+    will bleed over to other tests
     There's also a more advanced way to run code after each test as well
     Check the docs for that. Hint: it uses yield
     """
@@ -184,6 +177,23 @@ def clean():
 #######################
 # Stretch
 #######################
+
+
+# @pytest.mark.skip("stretch")
+# def test_class_tracks_instances():
+#     assert Band.to_list() == []
+#     the_nobodies = Band("The Nobodies", [])
+#     assert len(Band.instances) == 1
+#     assert Band.instances[0] == the_nobodies
+
+
+# @pytest.mark.skip("stretch")
+# def test_to_list():
+#     assert Band.to_list() == []
+#     the_nobodies = Band("The Nobodies", [])
+#     all_bands = Band.to_list()
+#     assert len(all_bands) == 1
+#     assert all_bands[0] == the_nobodies
 
 
 # @pytest.mark.skip("stretch")
@@ -212,4 +222,14 @@ def clean():
 # @pytest.mark.skip("stretch")
 # def test_abstract_musician():
 #     with pytest.raises(TypeError):
-#         unacceptably_vague_musician = Musician()
+#         Musician("nobody", "nothing", "silence")
+
+
+# @pytest.mark.skip("stretch")
+# def test_incomplete_keyboardist():
+#     with pytest.raises(TypeError) as e:
+#         Keyboardist("Booker T. Jones")
+
+#     assert (
+#         repr(e)
+#         == """<ExceptionInfo TypeError("Can't instantiate abstract class Keyboardist with abstract method some_method_that_must_be_implemented_in_base_class") tblen=1>"""  # noqa: E501
